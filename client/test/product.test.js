530e27c079ec179885447e1aac6af0508e0e3899
Ofir Katz <ofirkatz94@gmail.com>
2023-03-04 16:14:40 +0100

    test(product): add test for initial rendering

--- client/test/product.test.js
+++ client/test/product.test.js
@@ -1,5 +1,26 @@
 import { render, fireEvent } from "@testing-library/react";
+import { MockedProvider } from "@apollo/client/testing";
 import Product from "../pages/product";
+import { GET_PRODUCT, ProductBody } from "../pages/productBody";
+
+const mocks =  [
+  {
+    request: {
+      query: GET_PRODUCT,
+      variables: {
+        id: 1
+      }
+    },
+    result: {
+      "data": {
+          "Product": {
+              "id": "1",
+              "name": "Energy saving light bulb"
+          }
+      }
+    }
+  }
+];
 
 test("should be able to increase and decrease product quantity", async () => {
   const { getByText, getByTitle } = render(<Product />);
@@ -36,4 +57,15 @@ test("should be able to add items to the basket", async () => {
 
   const basketItems = getByTitle("Basket items");
   expect(basketItems).toHaveTextContent("4");
+});
+
+test("should render product name", async () => {
+  const { findByText } = render(
+    <MockedProvider mocks={mocks} addTypename={false}>
+      <ProductBody />
+    </MockedProvider>
+  );
+  expect(await findByText("Loading")).toBeInTheDocument();
+  const title = await findByText('Energy saving light bulb');
+  expect(title).toHaveTextContent('Energy saving light bulb');
 });
